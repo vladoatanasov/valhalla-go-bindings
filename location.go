8f4b133a7de9d3773295841a26368153af210504
Vladimir Atanasov <vlado.atanasov@gmail.com>
2018-07-10 10:18:48 +0100

    extended the location model

--- location.go
+++ location.go
@@ -1,7 +1,22 @@
 package valhalla
 
 type Location struct {
-	Lat    float64 `json:"lat,omitempty"`
-	Lon    float64 `json:"lon,omitempty"`
-	Street string  `json:"street,omitempty"`
+	Lat                 float64 `json:"lat,omitempty"`
+	Lon                 float64 `json:"lon,omitempty"`
+	Street              string  `json:"street,omitempty"`
+	Type                string
+	Heading             int
+	HeadingTolerance    int
+	MinimumReachability int
+	Radius              int
+	RankCandidates      bool
+	Name                string
+	City                string
+	State               string
+	PostalCode          string
+	Country             string
+	Phone               string
+	URL                 string
+	SideOfStreet        string //left, right
+	DateTime            string //2015-12-29T08:00
 }
